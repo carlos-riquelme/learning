2378621cd30a20bc1776e4f3dabc92ee0bae28f0
Carlos Riquelme <carlos.riquelme@gmail.com>
2018-05-10 19:42:17 -0300

    previo al push al repositorio learning_laravel/lecture 233

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -16,7 +16,7 @@
 <div class="form-group">
         
         {!! Form::label('category_id', 'Categoría:') !!}
-        {!! Form::select('category_id', array(1=>'PHP', 2=> 'JavaScript'), null, ['class'=>'form-control']) !!}
+        {!! Form::select('category_id', [''=>'Escoger'] + $categories, null, ['class'=>'form-control']) !!}
         
 </div>
 <div class="form-group">
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -24,7 +24,7 @@
                 <td>{{$post->id}}</td>
                 <td><img height="50" src="{{$post->photo ? $post->photo->file : '/images//50x50.png' }}" alt=""></td>
                 <td>{{$post->user->name}}</td>
-                <td>{{$post->category_id}}</td>
+                <td>{{$post->category ? $post->category->name : "Sin Categoría"}}</td>
                 {{--  <td><a href="{{route('admin.posts.edit', $post->id)}}">{{$post->title}}</a></td>  --}}
                 <td>{{$post->title}}</td>
                 <td>{{$post->body}}</td>
